4580684cf58d4a9d6003c6fef526d16fbe615bcb
Jousee <j.alan.falk@gmail.com>
2016-02-11 20:10:18 -0700

    Initial commit

---
+++ Assets/Jump.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Jump : MonoBehaviour {
+
+
+    public float jumpForce;
+    public Rigidbody2D sbody;
+
+	// Use this for initialization
+	void Start ()
+    {
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(Input.GetKeyDown( KeyCode.Space ) )
+        {
+            sbody.AddForce( new Vector2(0f, jumpForce), ForceMode2D.Impulse);
+        }
+	}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour {
+
+    public float moveL;
+    public float moveR;
+    public Rigidbody2D sbody;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            sbody.AddForce(new Vector2(-moveL, 0f));
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            sbody.AddForce(new Vector2(moveR, 0f));
+        }
+	}
+}
